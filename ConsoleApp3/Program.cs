a87a357bd17153ef9f3f65a268c3fa30b9a1edf0
Marin Maslov <32713180+marinmaslov@users.noreply.github.com>
2019-03-22 12:06:25 +0100

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helllo World!");
+            Console.WriteLine("Ja sam uvik ljutaaaaaaaaaaaaaaaaaaaaa!");
         }
     }
 }
