61f35fced8bb770b8469a7c27369ac286fdd22a5
leamladenic <33040005+leamladenic@users.noreply.github.com>
2019-03-29 10:45:55 +0100

    Create Class.cs

---
+++ ConsoleApp3/Class.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello World");
+        }
+    }
+}
